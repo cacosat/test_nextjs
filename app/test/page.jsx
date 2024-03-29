da6d3e0a5df625cb3025a8151f9aa4cfd85ab168
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-03-29 15:10:34 -0300

    update

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import { StrainerForm } from "@/components/component/strainer-form";
 
 export default function TestPage() {
     // hooks + helper functions
 
     return <>
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati, soluta doloribus sit ullam quos eligendi quasi nesciunt labore reiciendis aliquid cupiditate placeat perspiciatis, deserunt tempora sed cum est ipsa?</p>
             <Button>shadcn btn</Button>
+        </div>
+        <div className="max-w-[800px] mx-auto">
+            <StrainerForm />
         </div>
     </>
 }
