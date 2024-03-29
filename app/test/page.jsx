2e375b95e886d9f6ba3c00aa937a81c14c716159
Joaquin Sateler <86966872+cacosat@users.noreply.github.com>
2024-03-29 12:34:13 -0300

    testing shadcn integration

---
+++ app/test/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function TestPage() {
+    // hooks + helper functions
+
+    return <>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae obcaecati, soluta doloribus sit ullam quos eligendi quasi nesciunt labore reiciendis aliquid cupiditate placeat perspiciatis, deserunt tempora sed cum est ipsa?</p>
+            <Button>shadcn btn</Button>
+        </div>
+    </>
+}
